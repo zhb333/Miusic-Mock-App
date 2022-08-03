e52b6a12ca21e0ffc7cff03042b63840304b449d
zhb <1140457303@qq.com>
2022-08-03 18:45:53 +0800

    bindtap

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -7,16 +7,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topMVs: []
+    topMVs: [],
+    hasMore: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   async onLoad(options) {
-    const res = await getTopMV(0)
+    this.getTopMvData(0)
+  },
+
+  // 封装网络请求的方法
+  async getTopMvData(offset) {
+    if (!this.data.hasMore) return
+    // 展示加载动画
+    wx.showNavigationBarLoading()
+    const res = await getTopMV(offset)
+    let newData = this.data.topMVs
+    if (offset === 0) {
+      newData = res.data
+    } else {
+      newData = newData.concat(res.data)
+    }
     this.setData({
-      topMVs: res.data
+      topMVs: newData,
+      hasMore: res.hasMore
     })
+    wx.hideNavigationBarLoading()
+    if (this.offset === 0) {
+      wx.stopPullDownRefresh()
+    }
+  },
+  handleVideoItemClick(event) {
+    const id = event.currentTarget.dataset.item.id
+    console.log(id)
+  },
+  // 其他的生命周期回调函数
+  async onPullDownRefresh() {
+    this.getTopMvData(0)
+  },
+
+  async onReachBottom() {
+    this.getTopMvData(this.data.topMVs.length)
   },
 })
